88b8a8c2645d35c9207ab69d81549b98b63f3237
programmerTowsif <programmer.towsif@gmail.com>
2026-08-10 23:47:00 +0600

    optimazation sliding window convert o(n)

--- slidingWindow/basic.cpp
+++ slidingWindow/basic.cpp
@@ -9,6 +9,31 @@
 
 */
 
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n , k;
+//     cin>>n >>k ;
+//     int arr[n+1];
+//     for(int i =0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     for(int i =0;i< (n -k +1);i++){
+//         for(int j = i ;j<k+i;j++){
+//             cout<<arr[j]<<" ";
+//         }cout<<endl; // updated
+//     }
+// /*
+// input (n , k) 7 4
+// arr[i]       2 3 5 2 5 3 8
+// 1st window   2 3 5 2 
+// 2nd window     3 5 2 5 
+// 3rd window       5 2 5 3 
+// 4th windwo         2 5 3 8 
+// o(n^2)
+// */ 
+// }
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
@@ -18,11 +43,27 @@ int main(){
     for(int i =0;i<n;i++){
         cin>>arr[i];
     }
-    for(int i =0;i< (n -k +1);i++){
-        for(int j = i ;j<k+i;j++){
-            cout<<arr[j]<<" ";
-        }cout<<endl; // updated
-    }
+    // o(n**2)
+    // for(int i =0;i< (n -k +1);i++){
+    //     int sum =0;
+    //     for(int j = i ;j<k+i;j++){
+    //        sum+=arr[j];
+    //     }cout<<sum<<endl; // updated
+    
+   //}
+//    optimaization 
+// o(n) convert 
+int sum = 0;
+for(int i =0;i<k;i++) {
+    sum+=arr[i];
+}
+cout<<sum<<endl;
+for(int i = 0;i<n-k;i++){
+     
+    sum -=arr[i];
+    sum+=arr[k+i];
+    cout<<sum<<endl;
+}
 /*
 input (n , k) 7 4
 arr[i]       2 3 5 2 5 3 8
@@ -30,5 +71,6 @@ arr[i]       2 3 5 2 5 3 8
 2nd window     3 5 2 5 
 3rd window       5 2 5 3 
 4th windwo         2 5 3 8 
+o(n^2)
 */ 
 }
