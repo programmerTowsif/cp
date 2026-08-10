17f7f22edf8107f693f49c6c2872944b86cc74c8
programmerTowsif <programmer.towsif@gmail.com>
2026-08-10 10:24:41 +0600

    secode test

---
+++ frefix_sum/maxSubArry.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int>v;
+    for(int i =0;i<5;i++){
+        int x;
+        cin>>x;
+        v.push_back(x);
+    }
+    for(auto x:v)cout<<x<<" ";
+    return 0;
+}
--- frefix_sum/test.cpp
+++ frefix_sum/test.cpp
@@ -1,8 +1,6 @@
- 
-
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"hello hi towsif";
+    cout<<"hello hi towsif  df";
     return 0;
 }
